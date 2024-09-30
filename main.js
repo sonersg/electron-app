0f4837da8091b5233d9cc439d17b61584b6a9e72
Soner <sonersg@outlook.com>
2024-09-30 15:42:41 +0300

    final starter code

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("Soernr soner güçlü güçlü güçlü");
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+// calling your function when the app is ready
+app.whenReady().then(() => {
+  createWindow();
+});
+
+// Open a window if none are open (macOS)
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
